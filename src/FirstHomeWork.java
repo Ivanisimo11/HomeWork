e6fc89e977a14df26ee777f486cac1ded1937300
Ivanisimo11 <ivan.moshchenko3334@gmail.com>
2019-04-10 19:55:22 +0300

    human1 - анонимный класс ,human2 - лямбда

--- src/FirstHomeWork.java
+++ src/FirstHomeWork.java
@@ -1,5 +1,17 @@
 public class FirstHomeWork {
     public static void main(String[] args) {
-        System.out.println("fghjk");
+        Human human1 = new Human() {
+            public int calc(int x1, int y1) {
+                return x1 + y1;
+            }
+        };
+        int z1 = human1.calc(15,25);
+        System.out.println(z1);
+        Human human2 = (x2,y2)->x2+y2;
+        int z2 = human2.calc(15,15);
+        System.out.println(z2);
     }
+}
+interface Human{
+    int calc(int x,int y);
 }
