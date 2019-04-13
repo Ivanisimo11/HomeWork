1be181fd0596dd303e1288c8b41e110bb8982657
Ivanisimo11 <ivan.moshchenko3334@gmail.com>
2019-04-13 22:46:00 +0300

    домашнее задание после урока 2,добавлен пункт 1.4(факториал)!!!

--- src/SecondHomeWork.java
+++ src/SecondHomeWork.java
@@ -1,5 +1,47 @@
 public class SecondHomeWork {
     public static void main(String[] args) {
-        System.out.println("hjkl;");
+        Factorial f = new Factorial();
+        System.out.println(f.fact(6));
+
+    }
+}
+class Match{// _____________________1.1___________________________________
+    public int pariMatch(int schetPervoy,int schetVtoroy, int stavkaNaPervuy, int stavkaNaVtoruy){
+        int x =(schetPervoy == stavkaNaPervuy && schetVtoroy== stavkaNaVtoruy) ? 2: 0;
+        int y =(schetPervoy > schetVtoroy && stavkaNaPervuy > stavkaNaVtoruy || schetPervoy < schetVtoroy && stavkaNaPervuy < stavkaNaVtoruy || schetPervoy == schetVtoroy && stavkaNaPervuy== stavkaNaVtoruy) ? 1: 0;
+        if (x == 2){
+            return 2;
+        }else if (y == 1){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
+class Romb {//________________________________________1.2_________________________--
+    public void rombikusik(int diamondLength){
+        String stroka;
+        String stroka1;
+        for (int i =1;i<diamondLength;i++){
+            stroka = new String(new char[diamondLength - i]).replace("\0"," ");
+            stroka1 = new String(new char[2*i]).replace("\0","*");
+            System.out.println(stroka + stroka1);
+        }
+        for (int i =diamondLength;i!=0;i--){
+            stroka = new String(new char[diamondLength - i]).replace("\0"," ");
+            stroka1 = new String(new char[2*i]).replace("\0","*");
+            System.out.println(stroka + stroka1);
+        }
+    }
+}
+class Factorial {//_____________________________________1.4_____________________________
+
+    int fact(int n) {
+        int result;
+
+        if (n == 1)
+            return 1;
+        result = fact(n - 1) * n;
+        return result;
     }
 }
