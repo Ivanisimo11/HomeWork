1f1dcb9259bc67518c10b2209e2fb83d0539c2a8
Ivanisimo11 <ivan.moshchenko3334@gmail.com>
2019-04-14 19:26:27 +0300

    домашнее задание после урока 2,добавлен пункт 1.7(toString)

--- src/SecondHomeWork/TestMetodObjectClass.java
+++ src/SecondHomeWork/TestMetodObjectClass.java
@@ -1,30 +1,10 @@
 package SecondHomeWork;
-
-import java.util.Objects;
-
 public class TestMetodObjectClass {
-    int x =1;
-    int y =2;
 
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TestMetodObjectClass that = (TestMetodObjectClass) o;
-        return x == that.x &&
-                y == that.y;
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(x, y);
-    }
 }
 class Main1 {
     public static void main(String[] args) {
         TestMetodObjectClass testMetodObjectClass1 = new TestMetodObjectClass();
-        TestMetodObjectClass testMetodObjectClass2 = new TestMetodObjectClass();
-        boolean b = testMetodObjectClass1.equals(testMetodObjectClass2);
-        System.out.println(b);
+        System.out.println(testMetodObjectClass1.toString());
     }
 }
