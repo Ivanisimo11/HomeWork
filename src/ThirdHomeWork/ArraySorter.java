9109dc0caacd99a7b3186224e837217ff978edfd
Ivanisimo11 <ivan.moshchenko3334@gmail.com>
2019-04-21 17:01:44 +0300

    домашнее задание после урока 3(сортировка двух массивов(2 способ))

--- src/ThirdHomeWork/ArraySorter.java
+++ src/ThirdHomeWork/ArraySorter.java
@@ -1,25 +1,34 @@
 package ThirdHomeWork;
 
-import java.util.Arrays;
-
 public class ArraySorter {
     public int[] sorter(int [] a,int[]b){
         int n = a.length + b.length;
         int[] glava = new int[n];
-        for (int i = 0;i<a.length;i++){
-            glava[i]=a[i];
+        int y = 0;
+        int z = 0;
+        for (int i =0;i<n;i++){
+            if (y == b.length) {
+                glava[i]=a[z];
+                z++;
+            }else if (z == a.length){
+                glava[i]=b[y];
+                y++;
+            }
+            else if (b[y]> a[z]){
+                    glava[i]=a[z];
+                    z++;
+            }else {
+                    glava[i]=b[y];
+                    y++;
+                }
         }
-        for (int i = 0;i<b.length;i++){
-            glava[a.length +i]=b[i];
-        }
-        Arrays.sort(glava);
         return glava;
     }
 }
 class Main{
     public static void main(String[] args) {
-        int[] perviy = new int[]{1,3,4,7,3};
-        int[] vtoroy = new int[]{4,8,9,4,313};
+        int[] perviy = new int[]{1,10,20,30,765};
+        int[] vtoroy = new int[]{1,5,15,25,35,45};
         ArraySorter arraySorter = new ArraySorter();
         int[] t =arraySorter.sorter(perviy,vtoroy);
         for (int i = 0;i<t.length;i++){
